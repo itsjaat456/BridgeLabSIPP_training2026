e7637616ca748d98e7b5922298d624e5adb85bed
sachin kumar <sachinjaat2324@gmail.com>
2026-06-28 14:45:43 +0530

    [sachin kumar] .add assignment 1 quetions

---
+++ ConverKmToMiles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ConverKmToMiles {
+    static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter km = ");
+        int km = sc.nextInt();
+
+        System.out.println("km to miles = "+(km * 0.621371));
+    }
+}
